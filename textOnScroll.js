79b054cec45964845d06f7a32744a610956bcdef
Depanshu <depanshusahu057@gmail.com>
2023-09-03 03:36:37 +0530

    changes related to cursor

---
+++ textOnScroll.js
@@ -0,0 +1,23 @@
+// document.addEventListener("scroll", (event) => {
+//         console.log("scrolling")
+//     }
+// )
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   console.log("kuch toh hua")
+// }
+window.onscroll = function(){
+    // console.log("hua")
+    var scrollTop =  (document.documentElement || document.body.parentNode || document.body).scrollTop
+    // console.log(scrollTop)
+    let textBlocks = document.querySelectorAll(".side-texts-block");
+    for(let i =0;i<textBlocks.length ;i+=1){
+        if(i%2){
+            textBlocks[i].style.transform = "translateX( calc( -100px + " + scrollTop/10 + "px))";
+        }else{
+            textBlocks[i].style.transform = "translateX(-" + scrollTop/10 + "px)";
+        }
+    }
+}
+// console.log("working")
